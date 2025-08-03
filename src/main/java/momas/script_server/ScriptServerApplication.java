a8a76ac5eda5605b82f31ede6f8c2c6f0a1614c2
Sod-Momas <sothereer@gmail.com>
2025-08-03 16:33:58 +0800

    init repo

---
+++ src/main/java/momas/script_server/ScriptServerApplication.java
@@ -0,0 +1,13 @@
+package momas.script_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/momas/script_server/ScriptServerApplicationTests.java
@@ -0,0 +1,13 @@
+package momas.script_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
